6610515734c16517655981da34182135268a3378
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-07-30 09:07:08 +0500

    Add updateUser method to UserController for updating user data

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
@@ -14,6 +15,15 @@ export class UserController {
  @Post()
   createUser(@Body() CreateUserDto: CreateUserDto) {
     return {
+      data : CreateUserDto,
+      message: 'User created successfully',
+    };
+  };
+
+  @Put()
+  updateUser(@Body() UpdateUserDto: UpdateUserDto) {
+    return {
+      data : {id, ...UpdateUserDto},
       message: 'User created successfully',
     };
   }
