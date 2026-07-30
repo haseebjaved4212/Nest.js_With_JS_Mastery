ac3f14dda814993b3b5024ed51bba2514844af17
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-07-30 08:41:41 +0500

    Add getUsers method to UserController for retrieving user data

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+// Get user 
+ @Get()
+ getUsers() {
+  return[
+    {id: 1 , name: 'John Doe'},
+    {id: 2 , name: 'Haseeb'},
+  ];
+}
+
+
+
+
+
+
+}
