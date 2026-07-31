af1bcc2adab0a88e067f7d15eb9225c4025b2ec9
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-07-31 17:06:42 +0500

    Implement UserService with user data management and filtering functionality

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
 
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
 @Injectable()
-export class UserService {}
+export class UserService {
+  private users: User[] = [
+    { id: 1, name: 'John Doe', email: 'john.mclean@examplepetstore.com' },
+    { id: 2, name: 'Haseeb', email: 'haseeb@examplepetstore.com' },
+  ];
+
+  findAllUsers(name: string = '') {
+    return this.users.filter((user) =>
+      user.name.toLowerCase().includes(name.toLowerCase()),
+    );
+  }
+}
